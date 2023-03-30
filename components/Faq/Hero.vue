acee5d86e3fc8c03552c43efd926798471918908
Afolabi Timothy <afolabi.timothy51@gmail.com>
2023-03-30 01:22:09 +0100

    update on nyatwa project

--- components/Faq/Hero.vue
+++ components/Faq/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="background-color: #fff;">
       <div class="container support-bot">
-          <h2 class="header-faq">Frequently Asked Question</h2>
+          <h2 class="header-faq h">Frequently Asked Question</h2>
           <p class="support-text">Answers to some of your most asked questions about Nyatwa</p>
       </div>
     </div>
