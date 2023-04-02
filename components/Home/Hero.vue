9e6b77739c8c14f92ca12b7aff76e73cde33f192
Afolabi Timothy <afolabi.timothy51@gmail.com>
2023-04-02 22:12:30 +0100

    update

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -5,7 +5,7 @@
     <div class="hero-title">Move Money Freely</div>
     <div class="hero-text">Send and request money with a tap, purchase airtime, and shop online from anywhere in the world with our virtual dollar card.</div>
     <div class="d-flex justify-content-center mt-5">
-        <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn-download-hero"><span class=""><nuxt-img src="/images/DeviceMobileCamera.svg" class="img-fluid" alt="" srcset="" /></span>&nbsp; Download the app</div>
+        <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn-download-hero"><span class="pic-up"><nuxt-img src="/images/DeviceMobileCamera.svg" class="img-fluid" alt="" srcset="" /></span>&nbsp; Download the app</div>
     </div>
     <div class="money-s-1">
             <nuxt-img src="/images/hero-money-1.svg" class="img-fluid" alt="" srcset="" />
